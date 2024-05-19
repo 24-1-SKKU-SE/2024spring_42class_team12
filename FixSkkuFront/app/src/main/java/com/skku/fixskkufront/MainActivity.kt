e416b1c50e28d49aab16c3981617a5a29773f2f9
hjg1473 <hjg1473@naver.com>
2024-05-19 13:41:03 +0900

    Add Search, Filter

--- FixSkkuFront/app/src/main/java/com/skku/fixskkufront/MainActivity.kt
+++ FixSkkuFront/app/src/main/java/com/skku/fixskkufront/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.skku.fixskkufront
 
-import android.app.Dialog
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ListView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat

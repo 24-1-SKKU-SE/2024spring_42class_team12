3ab775dca3048964d5e344053d8262ba6a9a923a
alenjb <alenjb@naver.com>
2024-05-29 00:58:21 +0900

    [feat] #13 신고 조회 기능 완성

--- FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/report/controller/ReportController.java
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/report/controller/ReportController.java
@@ -8,14 +8,15 @@ import com.skku.fixskku.facility.domain.Facility;
 import com.skku.fixskku.facility.repository.FacilityRepository;
 import com.skku.fixskku.report.domain.Report;
 import com.skku.fixskku.report.dto.req.ReportReqDto;
+import com.skku.fixskku.report.dto.res.ReportListResDto;
 import com.skku.fixskku.report.dto.res.ReportResDto;
 import com.skku.fixskku.report.service.ReportService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/report")
@@ -27,6 +28,12 @@ public class ReportController {
         this.reportService = reportService;
         this.facilityRepository = facilityRepository;
     }
+
+    /**
+     * 신고기능
+     * @param requestDto 신고에 필요한 요청 객체
+     * @return API 명세서에 따른 응답
+     */
     @PostMapping
     public ResponseEntity<?> doReport(ReportReqDto requestDto){
         if (requestDto.getToken()== null || requestDto.getToken().isEmpty()) {
@@ -44,6 +51,32 @@ public class ReportController {
                     .build();
             long saveReportId = reportService.doReport(report);
             return ResponseApi.of(ResponseStatus._REPORT_SUCCESS, new ReportResDto(saveReportId));
+        }catch (GeneralException e) {
+            return ResponseApi.of(e.getStatus());
+        } catch (Exception e) {
+            return ResponseApi.serverError();
+        }
+    }
+
+    /**
+     * 자신의 신고 조회 기능
+     * @param token 조회할 사용자의 토큰 아이디
+     * @return API 명세서에 따른 응답
+     */
+    @GetMapping("/list")
+    public ResponseEntity<?> getAllReports(@RequestParam String token){
+        System.out.println(1);
+        if (token== null || token.isEmpty()) {
+            return ResponseApi.badRequest();
+        }
+        try {
+            Page<ReportListResDto> resultReports = reportService.getAllReports(token);
+            List<ReportListResDto> reports = resultReports.getContent();
+            Map<String, Object> result = new HashMap<>();
+            result.put("reports", reports);
+            result.put("page", resultReports.getNumber() + 1);
+            result.put("totalPages", resultReports.getTotalPages());
+            return ResponseApi.of(ResponseStatus._REPORT_LIST_SUCCESS, result);
         }catch (GeneralException e) {
             return ResponseApi.of(e.getStatus());
         } catch (Exception e) {

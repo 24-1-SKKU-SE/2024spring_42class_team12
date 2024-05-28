be77edbc868446eada778036f2b9e02d3d63cdcb
alenjb <alenjb@naver.com>
2024-05-29 00:55:32 +0900

    [feat] #13 자신의 신고 조회 응답 관련 DTO 생성

---
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/report/dto/res/ReportListResDto.java
@@ -0,0 +1,39 @@
+package com.skku.fixskku.report.dto.res;
+
+import com.skku.fixskku.report.domain.Report;
+import lombok.Data;
+
+@Data
+public class ReportListResDto {
+    private long reportId;
+    private long facilityId;
+    private String token;
+    private String description;
+    private String photoUrl;
+    private String reportStatus;
+    private String creationDate;
+    private String rejectionReason;
+    private String campus;
+    private String building;
+    private String floor;
+    private long classroom;
+    private String facilityType;
+    private String facilityStatus;
+
+    public ReportListResDto(Report report){
+        this.reportId = report.getId();
+        this.facilityId = report.getFacility().getId();
+        this.token = report.getTokenId();
+        this.description = report.getDescription();
+        this.photoUrl = report.getPhotoUrl();
+        this.reportStatus = report.getStatus().getName();
+        this.creationDate = report.getCreationDate().toString();
+        this.rejectionReason = report.getRejectionReason();
+        this.campus = report.getFacility().getCampusType().getCampusName();
+        this.building = report.getFacility().getBuildingType().getName();
+        this.floor = report.getFacility().getFloor();
+        this.classroom = report.getFacility().getClassroomNumber();
+        this.facilityType = report.getFacility().getType().getName();
+        this.facilityStatus = report.getFacility().getStatus().getName();
+    }
+}

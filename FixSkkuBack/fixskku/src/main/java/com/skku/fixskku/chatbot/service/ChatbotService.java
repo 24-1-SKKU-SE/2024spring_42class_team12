6c6f8ee6336416cbd34fd4f6a3ce3d132289f01f
alenjb <alenjb@naver.com>
2024-05-31 04:15:18 +0900

    [feat] #18 챗봇 일반 질문 서비스 기능 구현

--- FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/service/ChatbotService.java
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/service/ChatbotService.java
@@ -1,5 +1,7 @@
 package com.skku.fixskku.chatbot.service;
 
+import com.skku.fixskku.chatbot.dto.res.ChatbotResDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,4 +14,19 @@ public interface ChatbotService {
      * @param text faq 응답 내용
      */
     void faqRequest(String tokenId, long faqId, String text);
+
+    /**
+     * FAQ를 제외한 모든 챗봇 기능 요청을 챗봇으로 전달하고 응답을 API 명세에 맞게 가져오는 기능
+     * @param tokenId 사용자의 토큰 아이디
+     * @param text 사용자의 입력 텍스트
+     * @return API 명세에 따른 응답 객체
+     */
+    ResponseEntity<?> sendToChatbotAndGetAnswer(String tokenId, String text);
+
+    /**
+     * 챗봇 일반 질문 기능
+     * @param dto 챗봇으로부터 받은 응답 DTO
+     * @return 챗봇의 응답 DTO를 API 명세서에 맞게 바꾼 객체
+     */
+    ResponseEntity<?> returnNormalQuestion(ChatbotResDto dto);
 }
--- FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/service/ChatbotServiceImpl.java
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/service/ChatbotServiceImpl.java
@@ -2,11 +2,17 @@ package com.skku.fixskku.chatbot.service;
 
 import com.skku.fixskku.chatbot.constants.ChatbotUrl;
 import com.skku.fixskku.chatbot.dto.res.ChatbotResDto;
-import com.skku.fixskku.chatbot.dto.res.ToChatbotReqDto;
+import com.skku.fixskku.chatbot.dto.req.ToChatbotReqDto;
+import com.skku.fixskku.chatbot.dto.res.ToFrontResDto;
+import com.skku.fixskku.common.apipayload.ResponseApi;
+import com.skku.fixskku.common.apipayload.ResponseStatus;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 public class ChatbotServiceImpl implements ChatbotService{
@@ -26,6 +32,42 @@ public class ChatbotServiceImpl implements ChatbotService{
         // 요청 객체 생성
         HttpEntity<ToChatbotReqDto> request = new HttpEntity<>(reqDto, headers);
         restTemplate.postForEntity(ChatbotUrl.CHATBOT_SERVER, request, ChatbotResDto.class);
+    }
+
+    /**
+     * FAQ를 제외한 모든 챗봇 기능 요청을 챗봇으로 전달하고 응답을 API 명세에 맞게 가져오는 기능
+     * @param tokenId 사용자의 토큰 아이디
+     * @param text 사용자의 입력 텍스트
+     * @return API 명세에 따른 응답 객체
+     */
+    @Override
+    public ResponseEntity<?> sendToChatbotAndGetAnswer(String tokenId, String text) {
+        // 텍스트를 챗봇에게 전달
+        ToChatbotReqDto reqDto = new ToChatbotReqDto(text, null);
+        // 요청에 추가할 헤더 설정
+        HttpHeaders headers = setHttpHeaders(tokenId);
+        // 요청 객체 생성
+        HttpEntity<ToChatbotReqDto> request = new HttpEntity<>(reqDto, headers);
+        ResponseEntity<ChatbotResDto> chatbotResponse = restTemplate.postForEntity(ChatbotUrl.CHATBOT_SERVER, request, ChatbotResDto.class);
+        switch (Objects.requireNonNull(chatbotResponse.getBody()).getAnswerType()){
+            // 챗봇 일반 질문 기능
+            case NORMAL -> {
+                return returnNormalQuestion(chatbotResponse.getBody());
+            }
+
+        }
+
+        return null;
+    }
+
+    /**
+     * 챗봇 일반 질문 기능
+     * @param dto 챗봇으로부터 받은 응답 DTO
+     * @return 챗봇의 응답 DTO를 API 명세서에 맞게 바꾼 객체
+     */
+    public ResponseEntity<?> returnNormalQuestion(ChatbotResDto dto){
+        ToFrontResDto resDto = new ToFrontResDto(dto.getText(), null, null, null, null);
+        return ResponseApi.of(ResponseStatus._CHATBOT_NORMAL_SUCCESS,resDto);
     }
 
     /**

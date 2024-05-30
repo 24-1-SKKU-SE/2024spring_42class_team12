92ab88428002a9579bcf7533ba583fe2add669a2
alenjb <alenjb@naver.com>
2024-05-31 04:43:35 +0900

    [feat] #18 챗봇 URL 상수 추가

--- FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/constants/ChatbotUrl.java
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/constants/ChatbotUrl.java
@@ -2,4 +2,8 @@ package com.skku.fixskku.chatbot.constants;
 
 public final class ChatbotUrl {
     public static final String CHATBOT_SERVER = "https://beetle-prompt-finally.ngrok-free.app/chat/";
+    public static final String FACILITY_INFO_URI = ""; // 시설물 조회 URI
+    public static final String REPORT_URI = ""; // 신고 URI
+    public static final String MYREPORT_URI = ""; // 자신의 신고 조회 URI
+
 }

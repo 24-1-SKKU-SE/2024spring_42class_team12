f6d18abc21d970d96c6a01f872c56c9e1b5b9fda
yihwanggeun <wkwk0120@gmail.com>
2024-05-31 03:35:46 +0900

    Add MainActivity(Select User & Admin)

--- FixSkkuFront/app/src/main/java/com/skku/fixskkufront/MainActivity.kt
+++ FixSkkuFront/app/src/main/java/com/skku/fixskkufront/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.skku.fixskkufront
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +22,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val userButton: Button = findViewById<Button>(R.id.userBtn)
+        val adminButton: Button = findViewById<Button>(R.id.adminBtn)
+
+        userButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, CampusSelectActivity::class.java)
+            startActivity(intent)
+        }
+
+        adminButton.setOnClickListener {
+            showAdminDialog()
+        }
+    }
+
+    private fun showAdminDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Enter Admin Code")
+
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_NUMBER
+        builder.setView(input)
+
+        builder.setPositiveButton("Enter") { dialog, _ ->
+            val enteredCode = input.text.toString()
+            if (enteredCode == "1234") {
+                val intent = Intent(this@MainActivity, AdminActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@MainActivity, "Incorrect code", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+
+        builder.show()
     }
 }

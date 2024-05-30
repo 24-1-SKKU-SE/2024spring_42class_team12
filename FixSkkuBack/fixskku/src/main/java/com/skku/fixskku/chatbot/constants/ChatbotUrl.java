f852349255608abd212d21898b82002ebdc7aa6e
alenjb <alenjb@naver.com>
2024-05-31 03:09:20 +0900

    [feat] #18 챗봇 관련 상수와 enum 생성
    - 챗봇 응답 종류 enum 생성
    - 챗봇 응답 상태 추가
    - 챗봇 서버 URL 상수 추가

---
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/chatbot/constants/ChatbotUrl.java
@@ -0,0 +1,5 @@
+package com.skku.fixskku.chatbot.constants;
+
+public final class ChatbotUrl {
+    public static final String CHATBOT_SERVER = "https://beetle-prompt-finally.ngrok-free.app/chat/";
+}

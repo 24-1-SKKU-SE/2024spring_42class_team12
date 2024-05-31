f6aaecba6ac8023f2e33ddc60b3b8cb7876f2857
yo0919 <0919yo@naver.com>
2024-06-01 00:29:18 +0900

    #9 [chore] Facility 엔티티 수정

--- FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/facility/domain/Facility.java
+++ FixSkkuBack/fixskku/src/main/java/com/skku/fixskku/facility/domain/Facility.java
@@ -1,12 +1,14 @@
 package com.skku.fixskku.facility.domain;
 
 import com.skku.fixskku.common.domain.FacilityStatus;
+import com.skku.fixskku.common.domain.BuildingType;
+import com.skku.fixskku.common.domain.CampusType;
+import com.skku.fixskku.common.domain.FacilityType;
+import com.skku.fixskku.report.domain.Report;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,69 +23,28 @@ public class Facility {
     @Column(name = "facility_id")
     private long id;
 
+    @OneToMany(mappedBy = "facility")
+    private List<Report> reports;
+
+    @Column(name = "campus")
+    @Enumerated(EnumType.STRING)
+    private CampusType campusType;
+
+    @Column(name = "building")
+    @Enumerated(EnumType.STRING)
+    private BuildingType buildingType;
+
+    @Column(name = "floor")
+    private String floor;
+
     @Column(name = "classroom")
     private long classroomNumber;
 
-    @Column(name = "building")
-    private String building;
-
-    @Column(name = "campus")
-    private String campus;
+    @Column(name = "facility_type")
+    @Enumerated(EnumType.STRING)
+    private FacilityType type;
 
     @Column(name = "facility_status")
     @Enumerated(EnumType.STRING)
     private FacilityStatus status;
-
-    @Column(name = "facility_type")
-    private String type;
-
-    @Column(name = "floor")
-    private String floor;
-    public CampusType getCampusType() {
-        return new CampusType(campus);
-    }
-
-    public BuildingType getBuildingType() {
-        return new BuildingType(building);
-    }
-
-    public Type getType() {
-        return new Type(type);
-    }
-
-    public static class CampusType {
-        private final String campusName;
-
-        public CampusType(String campusName) {
-            this.campusName = campusName;
-        }
-
-        public String getCampusName() {
-            return campusName;
-        }
-    }
-
-    public static class BuildingType {
-        private final String name;
-
-        public BuildingType(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
-
-    public static class Type {
-        private final String name;
-
-        public Type(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
 }

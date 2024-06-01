eca98dc5a0be4c15256e4c3cfec7088bf7cb3ef8
hjg1473 <hjg1473@naver.com>
2024-06-01 19:35:57 +0900

    .

--- FixSkkuFront/app/src/main/java/com/skku/fixskkufront/AdminActivity.kt
+++ FixSkkuFront/app/src/main/java/com/skku/fixskkufront/AdminActivity.kt
@@ -84,7 +84,8 @@ class AdminActivity : AppCompatActivity() {
         val btnNew = findViewById<ImageButton>(R.id.new_btn)
         btnNew.setOnClickListener {
             myAdapter.updateList(item_init)
-            fetchAndParseJson("http://13.124.89.169/?reportStatus=fixed&startDate=&endDate=&searchWord=")
+            //fetchAndParseJson("http://13.124.89.169/?reportStatus=fixed&startDate=&endDate=&searchWord=")
+            fetchAndParseJson("http://10.0.2.2:8000/chatbot_test_get")
             btnInitPressed = false
             btnBeforePressed = false
             btnIngPressed = false
